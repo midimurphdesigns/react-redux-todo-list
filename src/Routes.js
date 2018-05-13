580063875caa2a202256bc034c1e14bb3e406d80
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-12 18:20:32 -0700

    routes js

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router.dom'
+
+import TodoContainer from './todos/containers/todoContainer'
+
+const Routes = (props) => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={TodoContainer} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export { Routes }
