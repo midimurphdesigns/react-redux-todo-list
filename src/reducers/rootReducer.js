17442fbb046cb5826e70381d0a69bc13fdb8968d
midimurphdesigns <midimurphdesigns@gmail.com>
2018-05-12 17:36:57 -0700

    configure store

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,3 +1,8 @@
 import {combineReducers} from 'redux'
 import {TodoListReducer} from '../todos/reducers/todoReducer'
 
+const rootReducer = combineReducers({
+  todos: TodoListReducer
+})
+
+export default rootReducer
